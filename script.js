26c5cdc9f11875d846406f31a7a5a807d0e4678b
Rebecca Wagaman <ribexy@gmail.com>
2017-08-03 01:02:55 +0000

    Loading basic google map with API key hidden

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // https://classroom.udacity.com/courses/ud864
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.741359, lng: -73.9980244},
+        zoom: 13
+    });
+}
