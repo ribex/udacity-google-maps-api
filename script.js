3a81d11c7d9cbde037ca88be9b2261300f354087
Rebecca Wagaman <ribexy@gmail.com>
2017-08-05 14:00:21 +0000

    Adding comments

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 var map;
 function initMap() {
+    // Constructor creates a new map: only center and zoom are required
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.741359, lng: -73.9980244},
+        // zoom range up to 21
         zoom: 13
     });
 }
