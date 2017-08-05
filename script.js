a516bda3e50c0d08ef5f8f2e0df66d3b92177743
Rebecca Wagaman <ribexy@gmail.com>
2017-08-05 14:28:58 +0000

    Remove single Tribeca location/marker/infowindow, and add locations array

--- script.js
+++ script.js
@@ -8,17 +8,28 @@ function initMap() {
         // zoom range up to 21
         zoom: 13
     });
-    var tribeca = {lat: 40.719526, lng: -74.0089934};
-    var marker = new google.maps.Marker({
-        position: tribeca,
-        map: map,
-        title: 'First Marker!'
-    });
-    var infowindow = new google.maps.InfoWindow({
-        content: 'Do you ever feel like an InfoWindow, floating through the wind,' +
-        ' ready to start again?'
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    })
+    // var tribeca = {lat: 40.719526, lng: -74.0089934};
+    // var marker = new google.maps.Marker({
+    //     position: tribeca,
+    //     map: map,
+    //     title: 'First Marker!'
+    // });
+    // var infowindow = new google.maps.InfoWindow({
+    //     content: 'Do you ever feel like an InfoWindow, floating through the wind,' +
+    //     ' ready to start again?'
+    // });
+    // marker.addListener('click', function() {
+    //     infowindow.open(map, marker);
+    // });
+    
+    // These are the real estate listings that will be shown to the user.
+    // Normally we'd have these in a database instead.
+    var locations = [
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}        
+    ];
 }
